e488aea505a54cd5d766172ad48b74cb1a18821e
Beyza <u.beyza.simsek@gmail.com>
2025-12-10 18:31:11 +0300

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,70 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import expressjs from "./tech/expressjs.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwindcss from "./tech/tailwindcss.png"; // DEĞİŞTİR !!!!!!!!!!
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import python from "./tech/python.png";
+
+import kos from "./company/kos.png";
+import yetgen from "./company/yetgen.png";
+import oua from "./company/oua.png";
+import aselsan from "./company/aselsan.png";
+import teknofest from "./company/teknofest.png"; // EKLE !!!!!!!!!!
+
+import KFAU_Academy from "./KFAU_Academy.png";
+import SHOPIX from "./SHOPIX.png";
+import Resumai from "./Resumai.png";
+import BYZAI from "./BYZAI.png";
+import Molix from "./Molix.png";
+import Foodix from "./Foodix.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  expressjs,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  tailwindcss,
+  tailwind,
+  typescript,
+  python,
+  kos,
+  yetgen,
+  oua,
+  aselsan,
+  teknofest,
+  KFAU_Academy,
+  SHOPIX,
+  Resumai,
+  BYZAI,
+  Molix,
+  Foodix
+};

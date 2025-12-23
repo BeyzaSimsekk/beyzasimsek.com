83b2fa96adc7d9f6a25aa445081585413f15d523
Beyza <u.beyza.simsek@gmail.com>
2025-12-23 16:46:40 +0300

    scroll down gif

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -150,6 +150,23 @@ const Hero = () => {
           </div>
         </div>
         <ComputersCanvas />
+
+        {/* Scroll Down Arrow */}
+        <div className="absolute bottom-8 right-8 z-20">
+          <a href="#about">
+            <div className="w-[35px] h-16 rounded-3xl border-4 border-secondary flex justify-center items-start p-2 hover:border-white transition-colors">
+              <motion.div
+                animate={{ y: [0, 24, 0] }}
+                transition={{
+                  duration: 1.5,
+                  repeat: Infinity,
+                  repeatType: "loop",
+                }}
+                className="w-3 h-3 rounded-full bg-secondary mb-1"
+              />
+            </div>
+          </a>
+        </div>
       </section>
     </>
   );
